6778ed06cda5bfea73d03269dd3ee9e71f9e6fe1
Raist90 <manuel.detoma@gmail.com>
2022-12-05 13:50:11 +0100

    added <title> with next/head

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,14 @@ import Cta from '../components/Cta'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import MobileHeader from '../components/MobileHeader'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Caffè Orsini</title>
+      </Head>
       <Header />
       <MobileHeader />
       <Banner />
